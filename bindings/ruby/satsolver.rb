0f6605e2a7eedb8d7148559267173f1d47915b5a
Duncan Mac-Vicar P <dmacvicar@suse.de>
2007-10-26 13:57:11 +0000

    Find libraries, and wrap the ruby native module into a ruby file

--- bindings/ruby/satsolver.rb
+++ bindings/ruby/satsolver.rb
@@ -1,7 +1,12 @@
 require 'satsolverx'
 
 module Satsolver
+  include Satsolverx
 
-
+  class Pool
+    def addsource_solv( fp, s);
+      pool_addsource_solv(self, fp, s);
+    end
+  end
 
 end
--- bindings/satsolver.i
+++ bindings/satsolver.i
@@ -1,4 +1,4 @@
-%module satsolver
+%module satsolverx
 
 %{
 
