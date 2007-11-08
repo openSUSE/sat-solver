07f4b99086fce4e5c508899fe2ddbc146841731e
Michael Schroeder <mls@suse.de>
2007-11-08 11:37:00 +0000

    - add a license - don't branch for supplemented packages - fix off by one in minimize queue revert code

--- testsuite/yps.c
+++ testsuite/yps.c
@@ -1,4 +1,11 @@
 /*
+ * Copyright (c) 2007, Novell Inc.
+ *
+ * This program is licensed under the BSD license, read LICENSE.BSD
+ * for further information
+ */
+
+/*
  * yps - without gimmick
  * 
  * command line interface to solver
