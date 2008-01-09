64f8e550a2cdcedc650b2ab64739843736332dc7
Klaus Kaempf <kkaempf@suse.de>
2008-01-09 12:31:35 +0000

    remove id2str (fix Action API !)

--- bindings/ruby/tests/transaction.rb
+++ bindings/ruby/tests/transaction.rb
@@ -24,7 +24,7 @@ class TransactionTest < Test::Unit::TestCase
     assert transaction.size == 6
     transaction.each { |a|
       cmd = case a.cmd
-            when SatSolver::INSTALL_SOLVABLE: "install"
+            when SatSolver::INSTALL_SOLVABLE: puts "install a."
 	    when SatSolver::REMOVE_SOLVABLE: "remove"
 	    when SatSolver::INSTALL_SOLVABLE_NAME: "install by name"
 	    when SatSolver::REMOVE_SOLVABLE_NAME: "remove by name"
@@ -32,7 +32,7 @@ class TransactionTest < Test::Unit::TestCase
 	    when SatSolver::REMOVE_SOLVABLE_PROVIDES: "remove by relation"
 	    else "<NONE>"
 	    end
-      puts "#{cmd}: #{pool.id2str(a.id)}"
+      puts "#{cmd}: #{a.id}"
     }
     transaction.clear!
     assert transaction.empty?
