4d0bdf44230dcb0882e5ebde24d1c6fdbe2ce6f2
Klaus Kaempf <kkaempf@suse.de>
2008-01-09 12:51:29 +0000

    add Action tests

--- bindings/ruby/tests/transaction.rb
+++ bindings/ruby/tests/transaction.rb
@@ -10,8 +10,6 @@ class TransactionTest < Test::Unit::TestCase
     pool.arch = "i686"
     repo = pool.add_solv( "../../../testsuite/data.libzypp/basic-exercises/exercise-1-packages.solv" )
     repo.name = "test"
-    puts "repo[0] #{repo[0]}"
-    puts "repo[1] #{repo[1]}"
     
     transaction = SatSolver::Transaction.new( pool )
     assert transaction
@@ -24,15 +22,15 @@ class TransactionTest < Test::Unit::TestCase
     assert transaction.size == 6
     transaction.each { |a|
       cmd = case a.cmd
-            when SatSolver::INSTALL_SOLVABLE: puts "install a."
-	    when SatSolver::REMOVE_SOLVABLE: "remove"
-	    when SatSolver::INSTALL_SOLVABLE_NAME: "install by name"
-	    when SatSolver::REMOVE_SOLVABLE_NAME: "remove by name"
-	    when SatSolver::INSTALL_SOLVABLE_PROVIDES: "install by relation"
-	    when SatSolver::REMOVE_SOLVABLE_PROVIDES: "remove by relation"
+            when SatSolver::INSTALL_SOLVABLE: "install #{a.solvable}"
+	    when SatSolver::REMOVE_SOLVABLE: "remove #{a.solvable}"
+	    when SatSolver::INSTALL_SOLVABLE_NAME: "install by name #{a.name}"
+	    when SatSolver::REMOVE_SOLVABLE_NAME: "remove by name #{a.name}"
+	    when SatSolver::INSTALL_SOLVABLE_PROVIDES: "install by relation #{a.relation}"
+	    when SatSolver::REMOVE_SOLVABLE_PROVIDES: "remove by relation #{a.relation}"
 	    else "<NONE>"
 	    end
-      puts "#{cmd}: #{a.id}"
+      puts cmd
     }
     transaction.clear!
     assert transaction.empty?
