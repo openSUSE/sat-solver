174123a6403ab97e8c53c80ec55aa50095566fd5
Klaus Kaempf <kkaempf@suse.de>
2008-08-04 12:49:57 +0000

    - start with Python examples by moving Ruby code - use Pythons 'unittest'

--- bindings/python/tests/loading.py
+++ bindings/python/tests/loading.py
@@ -1,3 +1,13 @@
+import unittest
+
 import sys
-sys.path.append('../../../build/bindings/python')
-import satsolver
+sys.path.insert(0, '../../../build/bindings/python')
+
+class TestSequenceFunctions(unittest.TestCase):
+    
+  def testloading(self):
+    import satsolver
+
+
+if __name__ == '__main__':
+  unittest.main()
