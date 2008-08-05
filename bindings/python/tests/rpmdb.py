d8a74c868dea0349974617e64bcfb9874c8656df
Klaus Kaempf <kkaempf@suse.de>
2008-08-05 10:52:21 +0000

    getting installed packages

---
+++ bindings/python/tests/rpmdb.py
@@ -0,0 +1,45 @@
+#
+# Load repository from rpm database
+#
+#   repo = pool.add_rpmdb( "/root/dir/of/system" )
+# will create an unnamed repository by reading the rpm database
+# and create a solvable for each installed package.
+# Use
+#   repo.name = "..."
+# to name the repository
+#
+#
+
+import unittest
+
+import sys
+sys.path.insert(0, '../../../build/bindings/python')
+
+import satsolver
+
+class TestSequenceFunctions(unittest.TestCase):
+    
+  def test_rpmdb(self):
+    pool = satsolver.Pool()
+    assert pool
+    pool.set_arch("i686")
+    repo = pool.add_rpmdb( "/" )
+    assert repo.size() > 0
+    print repo.size(), " installed packages"
+
+    name = None
+    for i in range(0, pool.size()):
+      s = pool.get(i)
+      print s
+      if i == 7:
+        name = s.name()
+      i += 1
+      if i > 10:
+        break
+
+    s = pool.find( name )
+    print "Seventh: ", name, " -> ", s
+
+
+if __name__ == '__main__':
+  unittest.main()
