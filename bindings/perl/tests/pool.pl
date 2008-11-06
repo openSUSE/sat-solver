fcc352b55f9d9e1b83d2bcec2a3ff724c136477b
Michael Schroeder <mls@suse.de>
2008-11-06 14:24:15 +0000

    - make it look more like perl

--- bindings/perl/tests/pool.pl
+++ bindings/perl/tests/pool.pl
@@ -8,11 +8,11 @@ use satsolver;
 # Test for 'Pool' class of sat-solver perl bindings
 #
 my $pool = new satsolver::Pool || die;
-$pool -> set_arch( 'i686' );
+$pool->set_arch('i686');
 
 # $pool->count_repos == 0
 # $pool->size == 1
 
 $pool->prepare();
 
-my $system = $pool->find( 'system:system' ) || die;
+my $system = $pool->find('system:system') || die;
