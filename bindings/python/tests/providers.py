72a5bf68fcef14e3effd697a2eb492bf219b0927
Klaus Kaempf <kkaempf@suse.de>
2008-12-03 17:44:36 +0000

    testcase for Python 'providers' iterator

--- bindings/python/tests/providers.py
+++ bindings/python/tests/providers.py
@@ -2,36 +2,36 @@
 # Check each_provider callback
 #
 
-$:.unshift ".."
+import unittest
 
-# test EachProvider
-require 'test/unit'
-require 'satsolver'
+import sys
+sys.path.insert(0, '../../../build/bindings/python')
 
-class EachProviderTest < Test::Unit::TestCase
-  def test_repo_create
-    pool = Satsolver::Pool.new
+import satsolver
+
+
+class TestSequenceFunctions(unittest.TestCase):
+    
+  def test_each_provider(self):
+    pool = satsolver.Pool()
     assert pool
-    pool.arch = "x86_64"
-    repo = pool.add_solv( "os11-biarch.solv" )
-    repo.name = "openSUSE 11.0 Beta3 BiArch"
-    puts "Repo #{repo.name} loaded with #{repo.size} solvables"
+    pool.set_arch("x86_64")
+    repo = pool.add_solv( "../../testdata/os11-biarch.solv" )
+    repo.set_name( "openSUSE 11.0 Beta3 BiArch" )
+    print "Repo ", repo.name(), " loaded with ", repo.size(), " solvables"
     system = pool.add_rpmdb( "/" )
-    system.name = "@system"
-    puts "Repo #{system.name} loaded with #{system.size} solvables"
-    
-    pool.prepare
+    system.set_name("@system")
+    print "Repo ", system.name(), " loaded with ", system.size(), " solvables"
 
-    puts "Providers of 'ispell_dictionary':"
-    pool.each_provider( "ispell_dictionary" ) { |s|
-      puts "  #{s} [#{s.repo.name}]"
-    }
-    
-    rel = pool.create_relation( "ispell_english_dictionary", Satsolver::REL_GT, "3.3.02-23" )
+    pool.prepare()
 
-    puts "Providers of #{rel}:"
-    pool.each_provider(rel) { |s|
-      puts "  #{s} [#{s.repo.name}]"
-    }
-  end
-end
+    i = 0
+    for solv in pool.providers("ispell_dictionary"):
+      print solv, " provides ispell_dictionary"
+
+    rel = pool.create_relation( "ispell_english_dictionary", satsolver.REL_GT, "3.3.02-23" )
+    for solv in pool.providers(rel):
+      print solv, solv.repo().name()
+      
+if __name__ == '__main__':
+  unittest.main()
