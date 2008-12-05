0ac0c7111ccfba8c95d0a07e3ffc33fafc33eab0
Klaus Kaempf <kkaempf@suse.de>
2008-12-05 11:21:39 +0000

    test per-repo search

--- bindings/ruby/tests/search.rb
+++ bindings/ruby/tests/search.rb
@@ -51,7 +51,7 @@ class RepodataTest < Test::Unit::TestCase
     repo.name = "openSUSE 11.0 Beta3 BiArch"
     puts "Repo #{repo.name} loaded with #{repo.size} solvables"
     
-    pool.search("yast2", Satsolver::SEARCH_STRING) do |d|
+    repo.search("yast2", Satsolver::SEARCH_STRING) do |d|
       puts "#{d.solvable} matches 'yast2' in #{d.key.name}: #{d.value}"
     end
   end
@@ -63,7 +63,7 @@ class RepodataTest < Test::Unit::TestCase
     solvpath = Pathname( File.dirname( __FILE__ ) ) + Pathname( "../../testdata" ) + "os11-biarch.solv"
     repo = pool.add_solv( solvpath )
     repo.name = "test"
-    pool.search("/usr/bin/python", Satsolver::SEARCH_STRING|Satsolver::SEARCH_FILES) do |d|
+    repo.search("/usr/bin/python", Satsolver::SEARCH_STRING|Satsolver::SEARCH_FILES) do |d|
       puts "#{d.solvable} matches '/usr/bin/python' in #{d.key.name}: #{d.value}"
     end
   end
