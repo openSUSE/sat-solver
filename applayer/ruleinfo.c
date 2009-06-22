f249859ff191689de4059b7b86e733c20da57248
Klaus Kämpf <kkaempf@suse.de>
2009-06-22 15:44:16 +0200

    More const

---
+++ applayer/ruleinfo.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2007, Novell Inc.
+ *
+ * This program is licensed under the BSD license, read LICENSE.BSD
+ * for further information
+ */
+
+/*
+ * Ruleinfo
+ *
+ * Information about a single solver rule.
+ *
+ */
+
+#include <stdlib.h>
+
+#include "ruleinfo.h"
+
+#include "applayer.h"
+
+
+Ruleinfo *
+ruleinfo_new( const Solver *solver, Id rule )
+{
+  Ruleinfo *ri = (Ruleinfo *)calloc( 1, sizeof( Ruleinfo ));
+  ri->solver = solver;
+  ri->cmd = solver_ruleinfo(solver, rule, &(ri->source), &(ri->target), &(ri->dep));
+  return ri;
+}
+
+
+void
+ruleinfo_free( Ruleinfo *ri )
+{
+  free( ri );
+}
+
+
+int
+command(const Ruleinfo *ri)
+{
+  return ri->cmd;
+}
+
+
+XSolvable *
+source(const Ruleinfo *ri)
+{
+  return ri->source ? xsolvable_new( ri->solver->pool, ri->source ) : NULL;
+}
+
+
+XSolvable *
+target(const Ruleinfo *ri)
+{
+  return ri->target ? xsolvable_new( ri->solver->pool, ri->target ) : NULL;
+}
+
+
+Relation *
+relation(const Ruleinfo *ri)
+{
+  return ri->dep ? relation_new( ri->solver->pool, ri->dep ) : NULL;
+}
